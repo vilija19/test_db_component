ee980c1106093474fc0d471ccb3d2d90814be07b
Alex <billibons777@gmail.com>
2022-05-14 09:40:40 +0000

    Add some function

--- src/Query.php
+++ src/Query.php
@@ -22,14 +22,36 @@ class Query implements \Aigletter\Contracts\Builder\QueryInterface
         $this->order = $order;
     }
 
+    /**
+     * Формирует строки для SQL запроса из ассоциативных массивов параметров
+     * @param array $assocArray - ассоциативный массив
+     * @param string $separator - склеивающая подстрока, включая пробелы если надо
+     * @return string 
+     */
+    protected function assocToStr(array $assocArray,string $separator):string
+    {
+        $sqlSubStr = '';
+        $i = 0;
+        foreach ($assocArray as $key => $value) {
+            $sqlSubStr .= $i ? $separator : '';            
+            $sqlSubStr .= $key . '=' . $value;
+            $i++;
+        }
+        return $sqlSubStr;
+    }
+
     public function toSql(): string
     {
+        $sql = '';
+        $start = $this->offset ? (int)$this->offset : 0;            
+        $limit = $this->limit ? (int)$this->limit : 20; 
+
         $sql =  'SELECT ' . implode(',' , $this->select);
         $sql .= ' FROM ' . $this->table . ' WHERE ';
-        foreach ($this->where as $key => $value) {
-            $sql .= $key . '=' . $value;
-        }
-
+        $sql .= $this->assocToStr($this->where, ' AND ');
+        $sql .= ' ORDER BY ';
+        $sql .= $this->assocToStr($this->order, ',');
+        $sql .= ' LIMIT ' . $start . "," . $limit;
 
         return $sql;
     }    
